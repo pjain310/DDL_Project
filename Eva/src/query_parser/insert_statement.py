baa53c476044820d1740773ff8dfa50ad058c4ad
arnavvj <ajeurkar3@gatech.edu>
2019-12-08 11:55:16 -0500

    Update insert_statement.py

--- Eva/src/query_parser/insert_statement.py
+++ Eva/src/query_parser/insert_statement.py
@@ -6,38 +6,27 @@ from src.query_parser.table_ref import TableRef
 from typing import List
 
 
-class SelectStatement(EvaStatement):
+class InsertStatement(EvaStatement):
     """
-    Select Statement constructed after parsing the input query
+    Insert Statement constructed after parsing the input query
     Attributes
     ----------
     _target_list : List[AbstractExpression]
-        list of target attributes in the select query,
+        list of target attributes in the insert query,
         each attribute is represented as a Abstract Expression
-    _from_table : TableRef
+    _to_table : TableRef
         table reference in the select query
-    _where_clause : AbstractExpression
-        predicate of the select query, represented as a expression tree.
     **kwargs : to support other functionality, Orderby, Distinct, Groupby.
     """
 
     def __init__(self, target_list: List[AbstractExpression] = None,
-                 from_table: TableRef = None,
-                 where_clause: AbstractExpression = None,
+                 to_table: TableRef = None,
                  **kwargs):
         super().__init__(StatementType.SELECT)
-        self._from_table = from_table
-        self._where_clause = where_clause
+        self._to_table = to_table
         self._target_list = target_list
 
-    @property
-    def where_clause(self):
-        return self._where_clause
-
-    @where_clause.setter
-    def where_clause(self, where_expr: AbstractExpression):
-        self._where_clause = where_expr
-
+    
     @property
     def target_list(self):
         return self._target_list
@@ -55,7 +44,7 @@ class SelectStatement(EvaStatement):
         self._from_table = table
 
     def __str__(self) -> str:
-        print_str = "SELECT {} FROM {} WHERE {}".format(self._target_list,
-                                                        self._from_table,
+        print_str = "INSERT INTO {} VALUES {}".format(self._target_list,
+                                                        self._to_table,
                                                         self._where_clause)
         return print_str
