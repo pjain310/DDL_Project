8d297f8aae4be2db7f557b5eee73083e246d746d
arnavvj <ajeurkar3@gatech.edu>
2019-12-09 15:57:12 -0500

    Update insert_statement.py

--- Eva/src/query_parser/insert_statement.py
+++ Eva/src/query_parser/insert_statement.py
@@ -16,14 +16,14 @@ class InsertStatement(EvaStatement):
         list of target attributes in the insert query,
         each attribute is represented as a Abstract Expression
     _to_table : TableRef
-        table reference in the select query
+        table reference in the insert query
     **kwargs : to support other functionality, Orderby, Distinct, Groupby.
     """
 
     def __init__(self, target_list: List[AbstractExpression] = None,
                  to_table: TableRef = None,
                  **kwargs):
-        super().__init__(StatementType.SELECT)
+        super().__init__(StatementType.INSERT)
         self._to_table = to_table
         self._target_list = target_list
 
