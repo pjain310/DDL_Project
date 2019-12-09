82305a329eb6c3d04aacd39772fdbda4ec464a16
arnavvj <ajeurkar3@gatech.edu>
2019-12-08 20:30:15 -0500

    Update insert_statement.py

--- Eva/src/query_parser/insert_statement.py
+++ Eva/src/query_parser/insert_statement.py
@@ -1,3 +1,4 @@
+# IMPORT MODULES
 from src.query_parser.eva_statement import EvaStatement
 from src.query_parser.types import StatementType
 from src.expression.abstract_expression import AbstractExpression
@@ -5,7 +6,7 @@ from src.loaders.abstract_loader import AbstractLoader
 from src.query_parser.table_ref import TableRef
 from typing import List
 
-
+# INIT CLASS FOR INSERT STATEMENT
 class InsertStatement(EvaStatement):
     """
     Insert Statement constructed after parsing the input query
@@ -26,23 +27,27 @@ class InsertStatement(EvaStatement):
         self._to_table = to_table
         self._target_list = target_list
 
-    
+    # DATA TO BE INSERTED
     @property
     def target_list(self):
         return self._target_list
     
+    # SET TARGET LIST
     @target_list.setter
     def target_list(self, target_expr_list: List[AbstractExpression]):
         self._target_list = target_expr_list
-
+        
+    # INSERT INTO THE FOLLOWING TABLE
     @property
     def to_table(self):
         return self._to_table
 
+    # SET TABLE
     @to_table.setter
     def to_table(self, table: TableRef):
         self._to_table = table
 
+    # SQL FOR INSERTING INTO EVA - PARSER    
     def __str__(self) -> str:
         print_str = "INSERT INTO {} VALUES {}".format(self._to_table,
                                                         self._target_list)
